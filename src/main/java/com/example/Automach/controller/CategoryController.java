6f712d0d0546c39a8e9aa5de3310d9e52ec3543e
yasaswini88 <anushagre2000@gmail.com>
2024-08-23 15:46:12 -0400

    added backemd code before tags

--- src/main/java/com/example/Automach/controller/CategoryController.java
+++ src/main/java/com/example/Automach/controller/CategoryController.java
@@ -1,2 +1,35 @@
-package com.example.Automach.controller;public class CategoryController {
+//package com.example.Automach.controller;
+//
+//public class CategoryController {
+//}
+package com.example.Automach.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.Automach.entity.Category;
+import com.example.Automach.Service.CategoryService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryService.getAllCategories();
+    }
+
+    @PostMapping
+    public Category createCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
+    }
 }
